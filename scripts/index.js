ec91c87f3916d2ea39c74a8d933542d34eac4ac0
Jhakailash <kailashjhaji7@gmail.com>
2022-04-25 12:28:35 +0530

    initial setup

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// Store the wallet amount to your local storage with key "amount"
+  
+function redirect(){
+    window.location.href="./movies.html"
+}
+function addmoney(){
+    let amount= +document.querySelector("#amount").value;
+
+     amount  =  amount + Number(localStorage.getItem("amount")|| 0);
+    localStorage.setItem("amount",amount);
+    document.querySelector("#wallet").innerHTML = amount;
+
+    document.querySelector("#amount").value = "";
+}
